f477c4530e634b912beb7c4d03ecb0f64223e75c
SaltyNumba1 <calebbeam2@gmail.com>
2023-11-04 10:33:27 -0700

    added js to slider- linkedin profile tag came back

---
+++ web project/js/Slider.js
@@ -0,0 +1,45 @@
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+  });
+
+  let curSlide = 0;
+
+  const nextSlide = document.querySelector(".btn-next");
+
+  nextSlide.addEventListener("click", function () {
+    curSlide++;
+
+ slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+      });
+    });
+//Need to find the value that keeps it from skipping 2 slides on next but prev works correctly 
+    let maxSlide = slides.length -1 ;
+
+    nextSlide.addEventListener("click", function () {
+        if (curSlide === maxSlide) {
+            curSlide = 0;
+          } else {
+            curSlide ++;
+          }
+          slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+          });
+        });
+
+        const prevSlide = document.querySelector(".btn-prev");
+
+    prevSlide.addEventListener("click", function () {
+        if (curSlide === 0) {
+            curSlide = maxSlide;
+          } else {
+            curSlide--;
+          }
+
+          slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+          });
+        });
+
