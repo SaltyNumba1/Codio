fc34b738300862ac14e04aa2e8b2832a3e5148d6
SaltyNumba1 <calebbeam2@gmail.com>
2023-11-04 17:17:12 -0700

    Added pages,moved main page, added slider

--- web project/js/Slider.js
+++ web project/js/Slider.js
@@ -6,19 +6,11 @@ slides.forEach((slide, indx) => {
 
   let curSlide = 0;
 
-  const nextSlide = document.querySelector(".btn-next");
+  let maxSlide = slides.length - 1;
 
-  nextSlide.addEventListener("click", function () {
-    curSlide++;
-
- slides.forEach((slide, indx) => {
-        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-      });
-    });
-//Need to find the value that keeps it from skipping 2 slides on next but prev works correctly 
-    let maxSlide = slides.length -1 ;
-
-    nextSlide.addEventListener("click", function () {
+    const nextSlide = document.querySelector(".btn-next");
+    
+      nextSlide.addEventListener("click", function () {
         if (curSlide === maxSlide) {
             curSlide = 0;
           } else {
@@ -29,13 +21,13 @@ slides.forEach((slide, indx) => {
           });
         });
 
-        const prevSlide = document.querySelector(".btn-prev");
+  const prevSlide = document.querySelector(".btn-prev");
 
-    prevSlide.addEventListener("click", function () {
+     prevSlide.addEventListener("click", function () {
         if (curSlide === 0) {
             curSlide = maxSlide;
           } else {
-            curSlide--;
+            curSlide --;
           }
 
           slides.forEach((slide, indx) => {
